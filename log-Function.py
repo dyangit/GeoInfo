064ec33402ed06f1ac33f03e2e47ed1f58353ac5
Melroy Dsouza <melroydsouza176@gmail.com>
2019-12-01 22:00:21 -0800

    Lambda Function for logging api

---
+++ log-Function.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    TABLE_NAME = "css436-Logging"
+    BUCKET_NAME = "css436-test"
+    ddb = boto3.client("dynamodb", region_name='us-west-2')
+    s3 = boto3.client("s3", region_name="us-west-2")
+
+    res = {
+        "Query-Requirement": "Please give a query with minimum required attribute: PID"
+    }
+    if event["queryStringParameters"] is not None:
+        item = s3.get_object(Bucket=BUCKET_NAME, Key="Input2.txt")
+        s3_update_string = ""
+        innerRequest = {'Item': {}}
+        for attribute in event["queryStringParameters"]:
+            s3_update_string = s3_update_string + attribute + ": " + event["queryStringParameters"][attribute] + " "
+            innerRequest["Item"][attribute] = {
+                 "S":event["queryStringParameters"][attribute]
+            }
+        res = ddb.put_item(TableName=TABLE_NAME, Item=innerRequest['Item'])
+        print(s3_update_string)
+        s3.put_object(Bucket=BUCKET_NAME, Key="Input2.txt", Body=item["Body"].read().decode('utf-8') + s3_update_string + "\n")
+    return {
+            'statusCode': 200,
+            'body': json.dumps(res)
+        }
