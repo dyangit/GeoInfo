5d303c8ed9363500fc212ff91e24858d9d50dc30
Daniel Yan <dyanonlineweb@gmail.com>
2019-12-02 00:14:35 -0800

    Modified server.js

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const appname = 'geoinfo';
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/' + appname + '>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/' + appname + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
