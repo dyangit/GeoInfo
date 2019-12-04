04116bca244f794eddd8b213b6e42bd134d88892
Melroy Dsouza <melroydsouza176@gmail.com>
2019-12-04 09:34:14 -0800

    Update log-Function.py

--- log-Function.py
+++ log-Function.py
@@ -12,16 +12,17 @@ def lambda_handler(event, context):
     }
     if event["queryStringParameters"] is not None:
         item = s3.get_object(Bucket=BUCKET_NAME, Key="Input2.txt")
-        s3_update_string = ""
+        s3_update_string = "username: " + event["queryStringParameters"]["username"] + " "
         innerRequest = {'Item': {}}
         for attribute in event["queryStringParameters"]:
-            s3_update_string = s3_update_string + attribute + ": " + event["queryStringParameters"][attribute] + " "
+            if(attribute != "username"):
+                s3_update_string = s3_update_string + attribute + ": " + event["queryStringParameters"][attribute] + " "
+                print(s3_update_string)
             innerRequest["Item"][attribute] = {
                  "S":event["queryStringParameters"][attribute]
             }
         res = ddb.put_item(TableName=TABLE_NAME, Item=innerRequest['Item'])
-        print(s3_update_string)
-        s3.put_object(Bucket=BUCKET_NAME, Key="Input2.txt", Body=item["Body"].read().decode('utf-8') + s3_update_string + "\n")
+        s3.put_object(Bucket=BUCKET_NAME, Key="Input2.txt", Body=item["Body"].read().decode('utf-8') + s3_update_string + "\n", ACL='public-read')
     return {
             'statusCode': 200,
             'body': json.dumps(res)
