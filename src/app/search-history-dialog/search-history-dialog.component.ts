1a823ec95d06b74ab24e7552bc633ae87c8e081b
Daniel Yan <dyanonlineweb@gmail.com>
2019-12-06 22:31:10 -0800

    Added filtering, sorting, and better table sizing for GET query

--- src/app/search-history-dialog/search-history-dialog.component.ts
+++ src/app/search-history-dialog/search-history-dialog.component.ts
@@ -3,6 +3,8 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 import { SearchResultData } from '../dialog-options/dialog-options.component';
 // Sorting and filtering has not been added as new table source has issues
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
   @Component({
   selector: 'app-search-history-dialog',
@@ -10,14 +12,16 @@ import { MatSort } from '@angular/material/sort';
   styleUrls: ['./search-history-dialog.component.scss']
 })
 export class SearchHistoryDialogComponent implements OnInit {
+  filterValue = '';
 
   displayedColumns: string[] = ['username', 'latitude', 'longitude', 'pid'];
-  dataSource = [];
+  dataSource =  new MatTableDataSource<SearchResultData>([]);
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: SearchResultData[]) { 
-    this.dataSource = this.data;
+    this.dataSource.data = this.data;
     //// Printing the value of the data source
     // this.dataSource.forEach(e => {
     //   console.log('value in datasource: ' + JSON.stringify(e));
@@ -25,5 +29,16 @@ export class SearchHistoryDialogComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  clearFilterValue(){
+    this.filterValue = '';
+    this.applyFilter(this.filterValue);
   }
 }
